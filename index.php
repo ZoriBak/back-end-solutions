6b9eb98402b1bb36717a75b71072671d5aa92c1c
ZoriBak <167759171+ZoriBak@users.noreply.github.com>
2025-02-19 16:27:30 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
-    
-    $firstName = "Zori";
-    $lastName = "Bak";
+   
+    $text = 'Test 123...';
+    $text2 = 'Test 456'; 
+    $_text3 = 'test';
+    $first_sentence = 'Building castles in the sky and in the sand'; 
+    $animal = "Platypus"; 
 
-  
-    $fullName = $firstName . " " . $lastName;
-
-    
-    $charCount = strlen($fullName);
 ?>
 
 <!DOCTYPE html>
-<html>
+<html> 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,10 +17,23 @@
     <link rel="stylesheet" type="text/css" href="/css/directory.css">
     <link rel="stylesheet" type="text/css" href="/css/facade.css">
 </head>
-<body>
-    <h1>Exercise: String Concatenation</h1>
 
-    <p>Full Name: <?php echo $fullName; ?></p>
-    <p>Character Count: <?php echo $charCount; ?></p>
+<body>
+
+    <h1>Debug errors</h1>
+
+    <ul>
+        <li>Debug this page so the script runs without error</li>
+        <li>Print all the variables in an unordered list below:</li>
+    </ul>  
+
+    <ul>
+        <li><?php echo $text; ?></li>
+        <li><?php echo $text2; ?></li>
+        <li><?php echo $_text3; ?></li>
+        <li><?php echo $first_sentence; ?></li>
+        <li><?php echo $animal; ?></li>
+    </ul>
+
 </body>
 </html>

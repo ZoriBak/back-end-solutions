c7b9d4b503ede918d2fa24125d494cecbf516145
ZoriBak <167759171+ZoriBak@users.noreply.github.com>
2025-03-04 22:18:22 +0100

    Add files via upload

---
+++ arrays-creation/index.php
@@ -0,0 +1,50 @@
+<?php
+$animalsNumerical = array('parrot', 'cat', 'spider', 'tiger', 'lion', 'giraffe', 'iguana', 'kangaroo', 'rabbit', 'frog');
+
+
+
+$animals = [];
+$animals[] ='parrot';
+$animals[] ='cat';
+$animals[] ='spider';
+$animals[] ='tiger';
+$animals[] ='lion';
+$animals[] ='giraffe';
+$animals[] ='iguana';
+$animals[] ='kangaroo';
+$animals[] ='rabbit';
+$animals[] ='frog';
+?>
+
+
+$vehicles = array(
+    "landVehicles" => array("Vespa"),
+    "waterVehicles" => array("surfboard"),
+    "airVehicles" => array("hot air balloon", "helicopter", "B52")
+);
+
+
+
+
+array(3){
+  ["landVehicles"]=>
+  array(2) {
+    [0]=> string(5) "Vespa"
+    [1]=> string(7) "bicycle"
+  }
+  ["waterVehicles"]=>
+  array(4) {
+    [0]=> string(9) "surfboard"
+    [1]=> string(4) "raft"
+    [2]=> string(8) "schooner"
+    [3]=> string(11) "three-master"
+  }
+  ["airVehicles"]=>
+  array(3) {
+    [0]=> string(14) "hot air balloon"
+    [1]=> string(9) "helicopter"
+    [2]=> string(3) "B52"
+  }
+}
+
+?>
